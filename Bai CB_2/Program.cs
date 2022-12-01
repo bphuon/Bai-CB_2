a8a6f09c0f800229057be7a6050da5ebb6d6891c
Phuong <phuonguyencv06@gmail.com>
2022-12-01 22:24:54 +0700

    Add project files.

---
+++ Bai CB_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_CB_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Nhap vao so nguyen duong");
+            switch (a=1)
+            {
+                case 1:
+                    Console.WriteLine("Mot");
+                    break;
+                case 2:
+                    Console.WriteLine("Hai");
+                    break;
+                case 3:
+                    Console.WriteLine("Ba");
+                    break;
+
+            }
+            
+        }
+        
+
+    }
+}
